c9329b6d9f1a2855e5bf0fb9bcdfd1a757c2a665
Pragyanta Dhal <pdhal@Pragyantas-MacBook-Air.local>
2021-11-01 20:11:48 -0400

    initial commit of testcases - N MapperRecuder

--- compile_run_test.sh
+++ compile_run_test.sh
@@ -1,5 +1,6 @@
 rm -rf runMapReduceTest
 mkdir runMapReduceTest
+rm -rf resources/Intermediate_files/
 javac -sourcepath src -d runMapReduceTest -cp lib/junit-platform-console-standalone-1.8.1.jar:. src/main/java/**/**/**/*.java src/test/java/*.java
 java -jar lib/junit-platform-console-standalone-1.8.1.jar --class-path runMapReduceTest --scan-class-path
 rm -rf runMapReduceTest
